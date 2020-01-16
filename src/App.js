4d2a564101273f5f57ccdf03d1fcf55e55e220f8
hjban <bananagga@naver.com>
2020-01-16 15:56:15 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -4,31 +4,35 @@ function Food({name, picture}){
 	return (
 		<div>
 			<h3>I like {name}!</h3>
-			<img src={picture} width="200" />
+			<img src={picture} alt={picture} width="200" />
 		</div>
 	)
 }
 
 const foodILike = [
 	{
+		id:1,
 		name: "chiken",
 		image:"http://www.bhc.co.kr/upload/bhc/menu/yang_view.JPG"
 	},
 	{
+		id:2,
 		name: "kimbap",
 		image:"https://migrationology.com/wp-content/uploads/2012/04/korean-street-food1.jpg"
 	},
 	{
+		id:3,
 		name: "pizza",
 		image:"https://media-cdn.tripadvisor.com/media/photo-s/15/c5/a4/14/pepperoni-lovers.jpg"
 	},
 ]
 
-
 function App() {
 	return (
 		<div>
-			{foodILike.map(current => <Food name={current.name} picture={current.image} />)}
+			{foodILike.map(current => 
+				<Food key={current.id} name={current.name} picture={current.image} />
+			)}
 		</div>
 	);
 }
