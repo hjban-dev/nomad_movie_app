491871b1395f788709b58bfd5dcd522c203d86d7
hjban <bananagga@naver.com>
2020-01-16 11:08:44 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import Potato from './Potato'
+
+function Food({name, picture}){
+	return (
+		<div>
+			<h3>I like {name}!</h3>
+			<img src={picture} width="200" />
+		</div>
+	)
+}
+
+const foodILike = [
+	{
+		name: "chiken",
+		image:"http://www.bhc.co.kr/upload/bhc/menu/yang_view.JPG"
+	},
+	{
+		name: "kimbap",
+		image:"https://migrationology.com/wp-content/uploads/2012/04/korean-street-food1.jpg"
+	},
+	{
+		name: "pizza",
+		image:"https://media-cdn.tripadvisor.com/media/photo-s/15/c5/a4/14/pepperoni-lovers.jpg"
+	},
+]
+
 
 function App() {
 	return (
 		<div>
-			<h1>hello!!!</h1>
-			<Potato />
+			{foodILike.map(current => <Food name={current.name} picture={current.image} />)}
 		</div>
 	);
 }
