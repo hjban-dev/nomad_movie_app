56d2e28d3457c6b55165d8f7ece3ff927dde1f1a
hjban <bananagga@naver.com>
2020-01-17 14:30:19 +0900

    #2.4 Protection with propTypes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React from 'react';
+import propTypes from 'prop-types'
 
-function Food({name, picture}){
+const foodILike = [
+	{
+		id: 1,
+		name: "chiken",
+		image: "http://www.bhc.co.kr/upload/bhc/menu/yang_view.JPG",
+		rating: 5
+	},
+	{
+		id: 2,
+		name: "kimbap",
+		image: "https://migrationology.com/wp-content/uploads/2012/04/korean-street-food1.jpg",
+		rating: 3.2
+	},
+	{
+		id: 3,
+		name: "pizza",
+		image: "https://media-cdn.tripadvisor.com/media/photo-s/15/c5/a4/14/pepperoni-lovers.jpg",
+		rating: 4.8
+	},
+]
+
+function Food({name, picture, rating}){
 	return (
 		<div>
 			<h3>I like {name}!</h3>
-			<img src={picture} alt={picture} width="200" />
+			<h4>{rating} / 5.0</h4>
+			{/* <img src={picture} alt={picture} width="200" /> */}
 		</div>
 	)
 }
 
-const foodILike = [
-	{
-		id:1,
-		name: "chiken",
-		image:"http://www.bhc.co.kr/upload/bhc/menu/yang_view.JPG"
-	},
-	{
-		id:2,
-		name: "kimbap",
-		image:"https://migrationology.com/wp-content/uploads/2012/04/korean-street-food1.jpg"
-	},
-	{
-		id:3,
-		name: "pizza",
-		image:"https://media-cdn.tripadvisor.com/media/photo-s/15/c5/a4/14/pepperoni-lovers.jpg"
-	},
-]
+Food.propTypes = {
+	name: propTypes.string.isRequired,
+	rating : propTypes.number.isRequired,
+	picture : propTypes.string.isRequired,
+};
 
 function App() {
 	return (
 		<div>
 			{foodILike.map(current => 
-				<Food key={current.id} name={current.name} picture={current.image} />
+				<Food 
+					key={current.id} 
+					name={current.name} 
+					picture={current.image} 
+					rating={current.rating}
+				/>
 			)}
 		</div>
 	);
