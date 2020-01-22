86410a2da5bab22031d1cc75e0be5872400c388c
hjban <bananagga@naver.com>
2020-01-22 17:42:01 +0900

    # 4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
-	constructor(props){
-		super(props)
-		console.log('constructor');
-	}
-	
 	state = {
-		count: 0
+		isLoading: true,
+		movies: [],
 	}
-	
-	add = () => {
-		this.setState({ count : this.state.count + 1 })
-	};
-	minus = () => {
-		this.setState( current => ({ count : current.count - 1 }))
-	};
-	
-	componentDidMount(){
-		console.log('componentDidMount')
-	}
-	componentDidUpdate(){
-		console.log('componentDidUpdate')
-	}
+
 	render(){
-		console.log('render');
+		const { isLoading, movies } = this.state;
 		return(
 			<div>
-				<h1>The number is {this.state.count}</h1>
-				<button onClick={this.add}>Add</button>
-				<button onClick={this.minus}>Minus</button>
+				{isLoading ? "Loading" : movies.map( movie => {
+					//console.log(movie)
+					return <Movie 
+								key = {movie.id} 
+								id = {movie.id} 
+								year = {movie.year} 
+								title = {movie.title} 
+								summary = {movie.summary} 
+								poster = {movie.medium_cover_image} 
+							/>;
+				})}
 			</div>
 		);
+	}
+	
+	getMovies = async() => {
+		const { data : { data : { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+		this.setState({ movies, isLoading: false})
+	}
+
+	componentDidMount(){
+		this.getMovies();
 	}
 }
 
