0eb57422cc7367d5fd616948c6d76ea95276d36e
hjban <bananagga@naver.com>
2020-01-28 15:35:03 +0900

    Updates

---
+++ precache-manifest.fa4cdf878689728b725ae5fea75df32f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e16d7f60cd8507052762e71ee47961",
+    "url": "/nomad_movie_app/index.html"
+  },
+  {
+    "revision": "89cc33570e86f89aa521",
+    "url": "/nomad_movie_app/static/css/main.07f15a69.chunk.css"
+  },
+  {
+    "revision": "0125782c355136ba5206",
+    "url": "/nomad_movie_app/static/js/2.90b3b815.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nomad_movie_app/static/js/2.90b3b815.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89cc33570e86f89aa521",
+    "url": "/nomad_movie_app/static/js/main.c8ba0e2d.chunk.js"
+  },
+  {
+    "revision": "c2e0260dc7bccd514b19",
+    "url": "/nomad_movie_app/static/js/runtime-main.3d5dc022.js"
+  }
+]);
