487c54486934b429038d1f09cbde8af85967e216
hjban <bananagga@naver.com>
2020-01-29 14:34:06 +0900

    6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './Movie.scss'
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 
-class App extends React.Component{
-	state = {
-		isLoading: true,
-		movies: [],
-	}
-
-	render(){
-		const { isLoading, movies } = this.state;
-		return(
-			<section className="container">
-				{isLoading 
-					? (
-						<div className="loader">
-							<p className="loader__text">Loading... </p>
-							<p className="loader__text">Wait a minute please...</p>	
-						</div> 
-					)
-					: (
-						<div className="movies">
-							{ movies.map( movie => {
-								return (
-									<Movie 
-										key = {movie.id} 
-										year = {movie.year} 
-										title = {movie.title} 
-										summary = {movie.summary} 
-										poster = {movie.medium_cover_image} 
-										genres = {movie.genres} 
-									/>
-								)
-							})}
-						</div>
-					)
-				}
-			</section>
-		);
-	}
-	
-	getMovies = async() => {
-		const { data : { data : { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-		this.setState({ movies, isLoading: false});
-	}
-
-	componentDidMount(){
-		this.getMovies();
-	}
+function App(){
+	return <HashRouter>
+		<Route path="/" exact={true} component={Home} />
+		<Route path="/about" component={About} />
+	</HashRouter>
 }
 
 export default App;
